302c9d97e39f0edaadfb331d486c31152b23a6fa
roktav <oktavio.rei@gmail.com>
2018-11-01 21:38:58 +0700

    latihan 2

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -4,13 +4,7 @@ public class Setting {
 
     public static final String pilotUrl = "https://b5f3a7f1-752e-4217-85a5-5951e6a5b14f.mock.pstmn.io";
 
-    private static final String AMADUES_API_KEY = "uiaunKvF5vSajCeGkAPvEJkNggsgR4A8";
+    final public static String airportUrl = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete";
 
-    public static final String AMADUES_API_URL = "https://api.sandbox.amadeus.com/v1.2";
-
-    public static final String AMADEUS_API_AIRPORT = AMADUES_API_URL + "/airports/autocomplete?apikey="+AMADUES_API_KEY;
-
-    public static final String getAirportIDUrlByCity(String city) {
-        return AMADEUS_API_AIRPORT + "&all_airports=true&country=ID&term=" + city;
-    }
+    final public static String airportApikey = "XGGFKpGY9DlooRAAqqQ9dRwn1zqXmTmE";
 }
